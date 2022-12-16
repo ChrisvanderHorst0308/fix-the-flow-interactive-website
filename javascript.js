d1513f21bd605c67f0e91e9f96d390554bc96144
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2022-12-16 13:16:47 +0100

    -

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let slidesContainer = document.getElementById('slides-container');
+let slide = document.querySelector('.slide');
+let prevButton = document.getElementById('slide-arrow-prev');
+let nextButton = document.getElementById('slide-arrow-next');
+let plusProgress = 
+
+nextButton.addEventListener('click', () => {
+    let slideWidth = slide.clientWidth;
+    
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+
+prevButton.addEventListener('click', () => {
+    let slideWidth = slide.clientWidth;
+
+  slidesContainer.scrollLeft -= slideWidth;
+});
